0367d4033e0323525d412f7a86f50a9e3273a547
MatiasPaisio110839 <69650056+MatiasPaisio110839@users.noreply.github.com>
2020-09-01 12:48:10 -0300

    Logging-Debugging

---
+++ ejemplo_modulo_request.py
@@ -0,0 +1,10 @@
+import requests
+request.get()
+res.status_code
+res.text
+res.raise_for_status()
+File = open('XdXD'. 'wb')
+
+for chunk in res.iter_content(100000):
+    playfile.write(chunk)
+playfile()
